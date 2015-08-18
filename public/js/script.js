c05d47a0b08e55639a054cfa20ce5013c01cd62f
Gene Lim <wernminlim@gmail.com>
2015-08-18 16:40:00 +0800

    MVP Completed

--- public/js/script.js
+++ public/js/script.js
@@ -3,18 +3,71 @@ angular.module('personal',['ngRoute'])
     $routeProvider
         .when('/', {
             templateUrl : '/views/home.html',
-            controller : ''
+            controller : 'HomeCtrl'
         })
         .when('/blog', {
             templateUrl : '/views/blog.html',
             controller : 'BlogCtrl'
-        });
+        })
+        .when('/work', {
+            templateUrl : '/views/work.html',
+            controller : 'BlogCtrl'
+        })
+        .when('/article/:id', {
+            templateUrl : '/views/article.html',
+            controller : 'ArticleCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
 })
-.controller('MainCtrl', function ($scope) {
+.controller('HomeCtrl', function ($scope,$http) {
 	$scope.heading = "Hey, Gene Lim here";
+    
+    $scope.contact = function(contacts){
+        if(contacts == undefined){
+            $('.message').html('Please fill in the required!');
+            $('.message').addClass('alert alert-danger');
+            $('.message').show();
+        }else if(!contacts.hasOwnProperty('names') || contacts.names == ''){
+            $('.message').addClass('alert alert-danger');
+            $('.message').html('Invalid Name!');
+            $('.message').show();
+        }else if(!contacts.hasOwnProperty('email') || contacts.email == ''){
+            $('.message').addClass('alert alert-danger');
+            $('.message').html('Invalid Email!');
+            $('.message').show();
+        }else if(!contacts.hasOwnProperty('message') || contacts.message == ''){
+            $('.message').addClass('alert alert-danger');
+            $('.message').html('Invalid Message!');
+            $('.message').show();
+        }else{
+            $http.post('/contact',contacts)
+            .success(function(data){
+                $('.message').addClass('alert alert-success');
+                $('.message').html('You message has be sent!');
+                $('.message').show();
+            });
+        }
+        setTimeout(function() {
+            $(".message").hide();
+        }, 2000);
+    };
+
+    $scope.skills_advance = [{  type:'HTML5'},{type:'CSS3'},{type:'JAVASCRIPT'},{type:'JQUERY'},{type:'ANGULARJS'},{
+                                type:'NODEJS'},{type:'MONGODB'},{type:'EXPRESS'},{type:'GITHUB'},{type:'PHOTOSHOP'},{
+                                type:'LIGHTROOM'},{type:'IONIC'}]
+
+     $scope.skills_experienced = [{   type:'WORDPRESS'},{type:'AJAX'},{type:'Csharp'},{type:'C',type:'CLI'},{type:'PHP'},{
+                                    type:'RAILS'},{type:'RUBY'},{type:'TORTOISE_SVN'},{type:'WORDPRESS'}];
+
+
 })
 .controller('NavCtrl', function ($scope) {
 	$(".hamburger").click(function () {
+        $('.sub-menu').toggle( "fast" );
+    });
+    $(".sub-menu ul li a").click(function () {
         $('.sub-menu').toggle( "fast" );
     });
 })
@@ -23,5 +76,15 @@ angular.module('personal',['ngRoute'])
 	.success(function(data) {
 		$scope.posts = data;
 	});
-
+})
+.controller('ArticleCtrl', function($scope, $http, $routeParams, $sce){
+    $http.get('/article/'+$routeParams.id)
+    .success(function(response){
+        $scope.article = response;
+        console.log($scope.article);
+    });
+    $scope.renderHtml = function(html_code)
+    {
+        return $sce.trustAsHtml(html_code);
+    };
 });
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	nodemailer = require('nodemailer');
+
+var user = 'username@gmail.com',
+	pass = 'password';
+
+//enable - https://www.google.com/settings/security/lesssecureapps , only it will work, IF WITHOUT XOAuth2. 
+var smtpTransport = nodemailer.createTransport("SMTP", {
+  	service: "Gmail",
+  	auth: {
+    	XOAuth2: {
+      		user: user, 
+      		clientId: "583098112938-bkjifegs0jdlbi88vot22alr4ltet235.apps.googleusercontent.com",
+      		clientSecret: 'PAX1Nixjbcng6T_Zl0TsR83T',
+      		refreshToken: "1/X8vH9lg8RM8aJATRe17EM4N94O0KVCE0nG_AVyijTLM"
+    	}
+  	}
+});
 
 mongoose.connect('mongodb://localhost/mean_blog');
 
@@ -18,8 +35,31 @@ app.use(bodyParser.json());
 
 app.get('/blog',function(request, response){
 	Post.find(function(err, post){
-		console.log(post);
 		response.json(post);
+	});
+});
+app.get('/article/:id', function(request, response){
+	Post.findOne({_id:request.params.id},function(err,post){
+		response.json(post);
+	});
+});
+
+app.post('/contact', function(request,response){
+	var mailOptions = {
+	    from: request.body.email, 
+	    to: user,
+	    subject: request.body.names, 
+	    generateTextFromHTML: true,
+	    html: request.body.message 
+	}
+	smtpTransport.sendMail(mailOptions, function(error, res){
+	    if(error){
+	        console.log(error);
+	    }else{
+	        console.log("Message sent: " + res.message);
+			response.json(res.message);
+	    }
+	    smtpTransport.close();
 	});
 });
 
